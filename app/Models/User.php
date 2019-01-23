2749b33e3931f1a9cdb16e87e25da4088b2ada89
Charlie JZ <jielin.zhang447@gmail.com>
2019-01-23 07:27:11 +0000

    6.2 show user info

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // gravatar, a global user avatar
+    public function gravatar($size = '100')
+    {
+      $hash = md5(strtolower(trim($this->attributes['email'])));
+      return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
