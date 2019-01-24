c16e436a8aabf4390f671c0aa180d6329e8f08e8
Charlie JZ <jielin.zhang447@gmail.com>
2019-01-24 10:56:14 +0000

    8.3 Auth and permission: Useing mutiple middlewares

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,6 +8,12 @@ use Auth;
 class SessionController extends Controller
 {
     //
+    public function __construct()
+    {
+      $this->middleware('guest', [
+        'only' => ['create']
+      ]);
+    }
     public function create()
     {
       return view('sessions.create');
@@ -24,7 +30,8 @@ class SessionController extends Controller
         // login successfully
         session()->flash('success', "欢迎回来，勇士。");
         // Auth::user() 方法来获取 当前登录用户 的信息
-        return redirect()->route('users.show', [Auth::user()]);
+        $fallback = route('users.show', Auth::user());
+        return redirect()->intended($fallback);
       }else {
         // Login error
         session()->flash('danger', '抱歉，您的用户名和密码不匹配。');
