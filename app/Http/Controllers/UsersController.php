882eeff8ccf93e05f2a61c12ae94b3ac5777acef
Charlie JZ <jielin.zhang447@gmail.com>
2019-01-29 08:10:50 +0000

    9.4: Config Email for production

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -90,12 +90,11 @@ class UsersController extends Controller
     {
       $view = 'emails.confirm';
       $data = compact('user');
-      $from = 'charlie.j.zhang@qq.com';
-      $name = 'Charlie';
       $to = $user->email;
       $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
-      Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject){
-        $message->from($from, $name)->to($to)->subject($subject);
+
+      Mail::send($view, $data, function ($message) use ($to, $subject){
+        $message->to($to)->subject($subject);
       });
     }
 
