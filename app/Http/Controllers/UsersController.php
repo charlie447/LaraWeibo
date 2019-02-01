9c04fe152d6baa76fb5aa962edcf838c4494a94c
Charlie JZ <jielin.zhang447@gmail.com>
2019-02-01 17:56:13 +0000

    11.6 followers' status showing dynamicly

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -119,4 +119,11 @@ class UsersController extends Controller
         $title = $user->name . '关注的人';
         return view('users.show_follow', compact('users', 'title'));
     }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '你的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
